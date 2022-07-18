5150d5f1c28b2c4eb4fc307f9d0872c3f6c7cff7
Matias Asenjo Pino <matia.cba74@gmail.com>
2022-07-18 00:09:43 -0300

    login

--- database.js
+++ database.js
@@ -2,10 +2,10 @@ const mysql=require("mysql")
 const util=require("util") 
 const pool=mysql.createPool({  //importa unas series de conexiones,host,database user etc.
     connectionLimit:10,
-    host: "localhost" ,
-    database:"users" ,
-    user: "root",
-    password: "",
+    host: process.env.db_host ,
+    database:process.env.db_name ,
+    user: process.env.db_user,
+    password: process.env.db_pass,
 
 })
 pool.query=util.promisify(pool.query) 
